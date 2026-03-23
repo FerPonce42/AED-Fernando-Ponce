f2d92ecc175e0a96c19b303226bc9d923030f6d8
FerPonce42 <ferponce042@gmail.com>
2026-03-23 08:59:50 -0500

    Tarea 1 Terminada

--- Tareas/1_ColaCircular.cpp
+++ Tareas/1_ColaCircular.cpp
@@ -1,54 +1,41 @@
 /*
-
 Tarea 1:
-Ahora haras una cola, que mrd es cola???
+Ahora haras una cola
+
 Cola controlada por un arreglo.
-El arreglo sera de n elementos. n, es ingresado por templates.
-Arr -> tipo de dato -> template (no necesariamente int, si no otros)
-
-Tendra un Head que apunte a la cabeza
-Y un Tail que apunta a la Cola
-
-FIRST INPUT, FIRST OUTPUT. El primero que entra, el primero que sale.
-
-Entra por la cola sale por la cabeza
-
-
+- El arreglo sera de n elementos. n, es ingresado por templates.
+- Arr -> tipo de dato -> template (no necesariamente int, si no otros)
+- Tendra un Head que apunte a la cabeza y un Tail que apunta a la Cola
+- FIRST INPUT, FIRST OUTPUT. El primero que entra, el primero que sale.
+- Entra por la cola, sale por la cabeza
 
 Atributos:
+- arr          -> elementos
+- nro_elemento -> igual que la cola
+- head = NULL  -> apunta siempre a la cabeza
+				  tras un pop el head avanza 1, de forma que ya no considera al primer elemento ingresado
+- tail = NULL  -> el tail se mueve
+				  en caso de meter un push teniendo el hueco del primer elemento,
+				  hacer que el tail vaya al primer elemento y sobreescriba al primer elemento.
+				  Si hay espacio, se reutiliza!!!
 
-arr -> elementos
-nro_elemento -> igual que la cola
-head = NULL el head apunta siempre cabeza
-tras un pop el head avanza 1, de forma que el head ya no considera al primer elemeno ingresado
-tail = NULL el tail se mueve
-
-
-en caso de meter un push teniendo el hueco del primer elemento. hacer que el tail vaya al primer elemenot y sobreescriba al primner elemento.
-Si hay espacio, se reutiliza!!!
-
-
-ESTA VACIO
-Cuando el H < T hay doble logica
-CUando el T < H
+Esta Vacio:
+- Cuando el H < T hay doble logica
+- Cuando el T < H
 
 Metodos:
-
-Constructor
-Destructor
-bool Lleno()
-bool Vacio()
-
-bool Push(T elemento) : METE POR LA OCLA
-
-bool Pop(T& elemento) : SACA POR LA CABEZA
-
-si llego a pop al utlimo elemento, todos apuntan a nullptr ya no hay mas p
+- Constructor
+- Destructor
+- bool Lleno()
+- bool Vacio()
+- bool Push(T elemento) : METE POR LA COLA
+- bool Pop(T& elemento) : SACA POR LA CABEZA
+						  si llego a pop al ultimo elemento, todos apuntan a nullptr, ya no hay mas p
 */
 #include <iostream>
 using namespace std;
 
-template<class T>
+template<class T, int N = 10>
 struct Cola {
 	int tamano;
 
@@ -56,7 +43,7 @@ struct Cola {
 	T* head = nullptr;
 	T* tail = nullptr;
 
-	Cola(int nro_tamano);
+	Cola();
 	~Cola();
 
 	bool EstaLleno();
@@ -66,63 +53,71 @@ struct Cola {
 };
 
 //Inicializar tamaño del arreglo.
-template<class T>
-Cola<T>::Cola(int nro_tamano) {
-	tamano = nro_tamano;
+template<class T, int N>
+Cola<T,N>::Cola() {
+	tamano = N;
 	arr = new T[tamano];
-	head = arr;
-	tail = arr;
+	head = nullptr;
+	tail = nullptr;
 }
 // Liberar tamaño
-template<class T>
-Cola<T>::~Cola() {
+template<class T, int N>
+Cola<T, N>::~Cola() {
 	delete[] arr;
 }
 
+/*
 
+FFUNCIONESS
 
-
-
-template<class T>
-bool Cola<T>::EstaVacio() {
+*/
+template<class T, int N>
+bool Cola<T, N>::EstaVacio() {
 	
-	if (tail == head) { // tail y head apuntan a lo mismo
+	if ((head == nullptr) && (tail == nullptr)) { // head y tail no apuntan a nada, SI ESTAVACIO
 		return true;
 	}
 	else {
-		return false;
+		return false; //NO VACIO
 	}
 
 }
 
 
-template<class T>
-bool Cola<T>::EstaLleno() {
+template<class T, int N>
+bool Cola<T, N>::EstaLleno() {
 	
-	if (tail + 1 == head) { //caso normal: si tail alcanzó a head
-		return true;
+	if (EstaVacio()) { // SI ESTA VACIO, NO ESTA LLENO.
+		return false;
 	}
-	else if ((tail == arr + tamano - 1) && (head == arr)) { //caso cuando dió una vuelta
+	else if (tail == head) { // TAIL alcanzó a HEAD. SI ESTA LLENO
 		return true;
 	}
 	else {
-		return false; // en caso de no estar lleno.
+		return false; //NO LLENO
 	}
 }
 
 
 // PUSH 
-template<class T>
-bool Cola<T>::Push(T valor) {
+template<class T, int N>
+bool Cola<T, N>::Push(T valor) {
 
-	if (EstaLleno()) {
+	if (EstaLleno()) { // SI esta lleno, no puedo hacer push.
 		return false;
 	}
-	else if ((tail == arr + tamano - 1)) {
-		*tail = valor;
+
+
+	if (EstaVacio()) { // Si esta vacio. inicializo al primer elemento.
+		head = arr;
 		tail = arr; 
+
 	}
-	else {
+	if(tail == arr + tamano - 1){ // Si tail esta al final, push valor y mando al inicio del arreglo.
+		*tail = valor;
+		tail = arr;
+
+	}else {// SI no esta en el final, inserto valor y avanzo (Caso normal)
 		*tail = valor;
 		tail++;
 	}
@@ -132,18 +127,29 @@ bool Cola<T>::Push(T valor) {
 }
 
 
+// POP
+template<class T, int N>
+bool Cola<T, N>::Pop(T& valor) {
 
-template<class T>
-bool Cola<T>::Pop(T& valor) {
-
-	if (EstaVacio()) {
+	if (EstaVacio()) { //Si Esta Vacio, No puedo POP
 		return false;
-	}else if ((head == arr + tamano - 1)) {
+	}
+
+	if(head == tail - 1){ // Si HEAD esta atras de TAIl, TERMINAMOS. 
+		valor = *head;
+		head = nullptr;
+		tail = nullptr;
+
+	}else if ((head == arr + tamano - 1) && (tail == arr)) { // SI HEAD llego al ultimo elemento Y Alcanzó a TAIL. TERMINAMOS
+		valor = *head;
+		head = nullptr;
+		tail = nullptr;
+	}else if(head == arr + tamano - 1){ //Si head llega al ultimo elemento, lo mandamos al inicio.
 		valor = *head;
 		head = arr;
-	}else{
+	}else {
 		valor = *head;
-		head++;
+		head ++; //Avanzamos head.
 	}
 
 	return true;
@@ -154,19 +160,21 @@ bool Cola<T>::Pop(T& valor) {
 
 int main() {
 
-	Cola<int> cola(4);
+	Cola<int,4> cola;
 
 	cola.Push(5);
 	cola.Push(7);
 	cola.Push(3);
 	cola.Push(1);
-
+	// [ 5, 7, 3 , 1 ];
 	int x;
 
 	cola.Pop(x);
 
+	// [ ignorado , 7, 3 , 1 ];
+
 	cola.Push(13);
 
-
+	// [ 13, 7, 3 , 1 ];
 
 }
