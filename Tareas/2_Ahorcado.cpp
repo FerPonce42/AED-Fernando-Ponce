4fa11e050f6c89416fb32ff9d23ccd2754c061bf
FerPonce42 <ferponce042@gmail.com>
2026-04-04 11:42:13 -0500

    Ahorcado Funcional Falta Ctrl Z

--- Tareas/2_Ahorcado.cpp
+++ Tareas/2_Ahorcado.cpp
@@ -1,122 +1,138 @@
+/*
+	TAREA 2: AHORCADO CON CTRL Z
+
+	Juego clasico del ahorcado con sistema de retroceso.
+
+	REGLAS BASE:
+	- 15 vidas, -1 por cada error
+	- 10 palabras de diferente longitud (max 30 caracteres)
+	- La palabra se elige al azar cada partida
+	- No es necesario dibujar el ahorcado
+
+	SISTEMA DE RETROCESO (Ctrl Z):
+	- Tecla especial: *
+	- Permite deshacer el ultimo movimiento (retrocede TODO lo del turno)
+	- Memoria de hasta 10 estados hacia atras
+	- Si llevas 11 movimientos, ya no puedes retroceder el primero
+	- Lo mas reciente se recuerda, lo mas lejano se olvida
+	- NO es pila, NO es cola. Estructura fija de 10 estados.
+
+	EL RETO:
+	Como guardar y restaurar los 10 estados del juego?
+*/
+
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
-/*Tarea Nueva:
-
-Tal cual el profesor:
-
-Típico juego del ahorcado…
-No es necesario dibujar.
-
-Vidas: 15
-Con cada error: -1 vida
-
-En una estructura de datos:
-10 palabras (de diferente longitud)
-Longitud máxima: 30
-
-Al azar en cada juego eliges una.
-
-EJEMPLO:
-
-Palabra X:
-
-Letra: ?
-
-Letra P
-P _ _ _ _
-
-Letra O
-P _ _ _ _
-No está, entonces vida -1
-P _ _ _ _
-
-O ← Se pinta esto para que el usuario no meta la pata.
-
-Vamos a hacer un juego con trampa, un ahorcado con Ctrl Z.
-
-¿Cómo así?
-Que el control Z tenga, no sé, una tecla *
-Con eso puedes retroceder en caso de que te equivocaste.
-
-Por ende, si te equivocaste, regresas.
-
-Ejemplo:
-TODO se retrocede, retrocede TODO lo que hiciste en ese movimiento.
-
-Básicamente el sistema va a memorizar 10 estados.
-Si haces 11 movimientos, ya no puedes retroceder más. O sea, tiene límite.
-
-EL RETO ES:
-¿Cómo haces para retroceder los estados?
-
-Nos dijo:
-10 palabras = 10 elementos del array
-
-Cosas a considerar:
-
-Si metes: A B C D E F G H I J K L M N…
-Si haces ese famoso control Z, vas retrocediendo esas veces que la cagaste.
-
-Te quedas en ABC, por lo que solo te acuerdas de C.
-
-Lo más lejano es lo que te olvidas.
-Lo más cercano es lo que recordarás.
-
-Básicamente recuerda 10 hacia atrás.
-
-NO es como pila.
-NO es como cola. Va por ahí.
-
-Invariable es eso de 10 estados, o sea que es un número fijo.*/
 
 
 struct Ahorcado {
-int vidas;
-string* palabras;
-int cant_estado;
-char letra;
+	int vidas;
+	string* arreglo_palabras;
+	int cant_estado;
+	char letra;
 
-Ahorcado();
-~Ahorcado();
+	string palabra_actual;
 
-void RecibirLetra(char letraopcion);
+	bool letra_acertada;
 
+	string palabra_progreso;
+
+	Ahorcado();
+	~Ahorcado();
+
+	bool RecibirLetra(char letraopcion);
+
+	void MarcarAciertos();
+
+	bool VerificarVictoria();
 
 };
 
-
 Ahorcado::Ahorcado() {
-vidas = 15;
-palabras = new string[10]{ "AMBIENTE", "FERROCARRIL", "CELULAR", "ELEFANTE", "UNIVERSIDAD", "SANPABLO", "CHOCOLATE", "PIZARRA", "MONITOR", "AHORCADO" };
-cant_estado = 10;
-letra = 'A';
+	vidas = 15;
+	arreglo_palabras = new string[10]{ "AMBIENTE", "FERROCARRIL", "CELULAR", "ELEFANTE", "UNIVERSIDAD", "SANPABLO", "CHOCOLATE", "PIZARRA", "MONITOR", "AHORCADO" };
+	cant_estado = 10;
+	letra = '\0';
 
+	palabra_actual = "";
+	letra_acertada = false;
+	palabra_progreso = "";
+
+	srand(time(NULL));
+	int indice = rand() % 10;
+	palabra_actual = arreglo_palabras[indice];
+
+	for (int i = 0; i < palabra_actual.length(); i++) {
+		palabra_progreso += "_";
+	}
 }
 
 Ahorcado::~Ahorcado() {
-delete[] palabras;
+	delete[] arreglo_palabras;
 }
 
+bool Ahorcado::RecibirLetra(char letraopcion) {
+	letra = letraopcion;
 
+	letra_acertada = false;
 
-void Ahorcado::RecibirLetra(char letraopcion) {
-letra = letraopcion;
-cout << "Letra Actual: "<< letra << endl;
+	for (int i = 0; i < palabra_actual.length(); i++) {
+		if (letra == palabra_actual[i]) {
+			letra_acertada = true;
+			palabra_progreso[i] = letra;
+		}
+	}
+
+	if (letra_acertada == false) {
+		vidas--;
+		cout << "Ups! -1 vida!" << endl;
+	}
+
+	return letra_acertada;
 }
 
+void Ahorcado::MarcarAciertos() {
+	cout << palabra_progreso << endl;
+}
 
+bool Ahorcado::VerificarVictoria() {
+	for (int i = 0; i < palabra_progreso.length(); i++) {
+		if (palabra_progreso[i] == '_') {
+			return false;
+		}
+	}
 
-
-
-
+	return true;
+}
 
 int main() {
 
-Ahorcado juego1;
+	Ahorcado juego1;
 
-char letraopcion;
-cout << "Letra: "; cin >> letraopcion;
-juego1.RecibirLetra(letraopcion);
+	char letraopcion;
 
+	cout << "\n===============================" << endl;
+	cout << "         AHORCADO" << endl;
+	cout << "===============================" << endl;
+	cout << "Palabra: " << juego1.palabra_progreso << endl;
+
+	while ((juego1.vidas > 0) && (juego1.VerificarVictoria() == false)) {
+		cout << "\n-------------------------------" << endl;
+		cout << "Vidas: " << juego1.vidas << " / 15" << endl;
+		cout << "Ingresa una letra: "; cin >> letraopcion;
+		juego1.RecibirLetra(letraopcion);
+		cout << "Palabra: ";
+		juego1.MarcarAciertos();
+	}
+
+	cout << "\n===============================" << endl;
+	if (juego1.VerificarVictoria()) {
+		cout << "GANASTE! La palabra era: " << juego1.palabra_actual << endl;
+	}
+	else {
+		cout << "PERDISTE! La palabra era: " << juego1.palabra_actual << endl;
+	}
+	cout << "===============================" << endl;
 }
