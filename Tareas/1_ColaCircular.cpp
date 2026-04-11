cdea071fc91846b74884ec4151bff80a70f879ef
FerPonce42 <ferponce042@gmail.com>
2026-04-10 22:57:17 -0500

    Cola Circular, version correcta, la anterior era un borrador tmrrrr ahfasdada

--- Tareas/1_ColaCircular.cpp
+++ Tareas/1_ColaCircular.cpp
@@ -50,9 +50,10 @@ struct Cola {
 	bool EstaVacio();
 	bool Push(T valor);
 	bool Pop(T& valor);
+
+	void Print();
 };
 
-//Inicializar tamaño del arreglo.
 template<class T, int N>
 Cola<T,N>::Cola() {
 	tamano = N;
@@ -60,7 +61,7 @@ Cola<T,N>::Cola() {
 	head = nullptr;
 	tail = nullptr;
 }
-// Liberar tamaño
+
 template<class T, int N>
 Cola<T, N>::~Cola() {
 	delete[] arr;
@@ -86,11 +87,11 @@ bool Cola<T, N>::EstaVacio() {
 
 template<class T, int N>
 bool Cola<T, N>::EstaLleno() {
-	
-	if (EstaVacio()) { // SI ESTA VACIO, NO ESTA LLENO.
+
+	if (EstaVacio()) { // SI ESTA VACIO, NO ESTA LLENO. (no criticar)
 		return false;
 	}
-	else if (tail == head) { // TAIL alcanzó a HEAD. SI ESTA LLENO
+	else if (tail + 1 == head || (tail == arr + tamano - 1 && head == arr)) { // TAIL alcanzó a HEAD. SI ESTA LLENO
 		return true;
 	}
 	else {
@@ -107,20 +108,20 @@ bool Cola<T, N>::Push(T valor) {
 		return false;
 	}
 
-
 	if (EstaVacio()) { // Si esta vacio. inicializo al primer elemento.
 		head = arr;
-		tail = arr; 
-
-	}
-	if(tail == arr + tamano - 1){ // Si tail esta al final, push valor y mando al inicio del arreglo.
-		*tail = valor;
 		tail = arr;
-
-	}else {// SI no esta en el final, inserto valor y avanzo (Caso normal)
-		*tail = valor;
-		tail++;
+		*tail = valor; // Inserto y me quedo aqui, tail apunta al elemento insertado
+		return true;
 	}
+
+	if (tail == arr + tamano - 1) { // tail esta al final, lo mando al inicio
+		tail = arr;
+	} else {
+		tail++; // Avanzo al siguiente hueco
+	}
+
+	*tail = valor; // Inserto en la nueva posicion
 
 	return true;
 
@@ -135,21 +136,16 @@ bool Cola<T, N>::Pop(T& valor) {
 		return false;
 	}
 
-	if(head == tail - 1){ // Si HEAD esta atras de TAIl, TERMINAMOS. 
+	if (head == tail) { // Solo queda un elemento, TERMINAMOS.
 		valor = *head;
 		head = nullptr;
 		tail = nullptr;
-
-	}else if ((head == arr + tamano - 1) && (tail == arr)) { // SI HEAD llego al ultimo elemento Y Alcanzó a TAIL. TERMINAMOS
-		valor = *head;
-		head = nullptr;
-		tail = nullptr;
-	}else if(head == arr + tamano - 1){ //Si head llega al ultimo elemento, lo mandamos al inicio.
+	} else if (head == arr + tamano - 1) { // head llega al final, regreso al inicio
 		valor = *head;
 		head = arr;
-	}else {
+	} else {
 		valor = *head;
-		head ++; //Avanzamos head.
+		head++;
 	}
 
 	return true;
@@ -157,24 +153,53 @@ bool Cola<T, N>::Pop(T& valor) {
 }
 
 
+// PRINT
+template<class T, int N>
+void Cola<T, N>::Print() {
+
+	if (EstaVacio()) {
+		cout << "[ ]" << endl;
+		return;
+	}
+
+	T* ptr = head;
+	cout << "[ ";
+
+	while (ptr != tail) {
+		cout << *ptr << " ";
+		if (ptr == arr + tamano - 1) ptr = arr; 
+		else ptr++;
+	}
+
+	cout << *ptr << " ]" << endl; 
+
+}
 
 int main() {
 
-	Cola<int,4> cola;
+	Cola<int, 5> cola;
 
-	cola.Push(5);
-	cola.Push(7);
-	cola.Push(3);
 	cola.Push(1);
-	// [ 5, 7, 3 , 1 ];
+	cola.Push(2);
+	cola.Push(5);
+	cola.Push(4);
+	cola.Push(8);
+	cola.Print();
+	// [ 1 2 5 4 8 ]
+
 	int x;
-
 	cola.Pop(x);
+	cola.Pop(x);
+	cola.Pop(x);
+	cola.Pop(x);
+	cola.Print();
+	// [ 8 ]
 
-	// [ ignorado , 7, 3 , 1 ];
-
-	cola.Push(13);
-
-	// [ 13, 7, 3 , 1 ];
+	cola.Push(50);
+	cola.Push(23);
+	cola.Push(16);
+	cola.Push(15);
+	cola.Print();
+	// [ 8 50 23 16 15 ]
 
 }
