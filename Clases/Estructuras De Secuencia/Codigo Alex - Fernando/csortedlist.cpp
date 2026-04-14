54d697cc1ed7c83703fdfde088e61d9b71ed3f3b
FerPonce42 <ferponce042@gmail.com>
2026-04-13 21:15:39 -0500

    Csorted list ready to plaayy

--- Clases/Estructuras De Secuencia/Codigo Alex - Fernando/csortedlist.cpp
+++ Clases/Estructuras De Secuencia/Codigo Alex - Fernando/csortedlist.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-using namespace std; 
-
+using namespace std;
 
 struct CNode
 {
     CNode(int v)
-    {   value = v; next = 0;    }
+    {
+        value = v; next = 0;
+    }
     int value;
     CNode* next;
 };
@@ -24,40 +25,106 @@ private:
 };
 
 CSortedList::CSortedList()
-{   head = 0;   }
+{
+    head = 0;
+}
 
 CSortedList::~CSortedList()
 {
-  
+    CNode* limpiar = head;
+    
+    while (limpiar != nullptr) {
+        CNode* tmp = limpiar->next;
+        delete limpiar;
+
+        limpiar = tmp;
+    }
+
 }
 
 bool CSortedList::find(int x, CNode**& p)
 {
+    p = &head;
+
+    while (*p != nullptr && x > ((*p)->value)) {
+
+        p = &((*p)->next);
+
+    }
+
+    return *p != nullptr && x == ((*p)->value);
 
 }
 
 bool CSortedList::ins(int x)
 {
+    CNode** p;
 
+    if (find(x,p)) {
+        return false;
+    }
+    else
+    {
+        CNode* nodo = new CNode(x);
+        nodo->value = x;
+        nodo->next = *p;
+
+        *p = nodo;
+    }
+
+    return true;
 }
 
 bool CSortedList::rem(int x)
 {
+    CNode** p;
 
+    if (find(x, p)) {
+        
+        CNode* tmp;
+        tmp = (*p)->next;   
+        delete* p;        
+        *p = tmp;          
+        
+    }
+    else
+    {
+        return false;
+    }
+
+    return true;
 }
 
 void CSortedList::print()
 {
-  
+    CNode* mover = head;
+
+    if (mover == nullptr) {
+        cout << "Vacio" << endl;
+    }
+
+    for (mover; mover != nullptr; mover = mover->next) {
+        cout << mover->value << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
     CSortedList l;
-    l.ins(3); l.ins(14); l.ins(7); l.ins(11);
+    l.ins(3); // 3
+    l.ins(14); // 3 14
+    l.ins(7); // 3 14 7
+    l.ins(11);// 3 14 7 11
+    l.print(); // 3 7 11 14
+
+
+    l.rem(11);  // 3 7 14
     l.print();
-    l.rem(11); l.print();
-    l.rem(14); l.print();
-    l.rem(3); l.print();
-    l.rem(7); l.print();
+    l.rem(14); // 3 7 
+    l.print();
+    l.rem(3); // 7 
+    l.print();
+    l.rem(7); // Vacio
+    l.print();
 }
